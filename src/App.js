2f568089772092ba2da7d0f5277ab4fbb13b4da8
christobal <cristobal.silva@riotsecure.se>
2020-11-21 20:41:31 -0300

    countDown component created with working logic and basic styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { CountDown } from './CountDown';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <CountDown timeTillDate="12 21 2020, 6:00 am" timeFormat="MM DD YYYY, h:mm a" />
+    );
+  }
 }
 
 export default App;
